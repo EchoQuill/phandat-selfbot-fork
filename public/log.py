84f9a6fa4702b3c204c58b77c909f704b92c570e
realphandat <155445899+realphandat@users.noreply.github.com>
2024-09-18 18:24:11 +0700

    Add files via upload

--- public/log.py
+++ public/log.py
@@ -1,25 +1,14 @@
 import logging, logging.handlers
 
-from public.color import Color
-
 class CustomFormatter(logging.Formatter):
-	console_template = "\x1b[43m%(asctime)s\x1b[0m - \x1b[38;5;212m%(name)s\x1b[0m - {}[%(levelname)s]{} %(message)s"
-	file_template = "%(asctime)s - %(name)s - [%(levelname)s] %(message)s"
-
-	formats = {
-		logging.DEBUG: console_template.format(Color.gray, Color.reset) + " (%(filename)s : %(lineno)d)",
-		logging.INFO: console_template.format(Color.blue, Color.reset),
-		logging.WARNING: console_template.format(Color.yellow, Color.reset),
-		logging.ERROR: console_template.format(Color.red, Color.reset) + " (%(filename)s : %(lineno)d)",
-		logging.CRITICAL: console_template.format(Color.purple, Color.reset) + " (%(filename)s : %(lineno)d)",
-	}
+	template = "%(asctime)s - %(name)s - [%(levelname)s] %(message)s"
 
 	def __init__(self, is_file = False):
 		super().__init__(datefmt = "%d %b %Y %H:%M:%S")
 		self.is_file = is_file
 
 	def format(self, record):
-		log_fmt = self.file_template if self.is_file else self.formats.get(record.levelno)
+		log_fmt = self.template
 		formatter = logging.Formatter(fmt = log_fmt, datefmt = self.datefmt)
 		return formatter.format(record)
 
@@ -36,7 +25,7 @@ class Log:
 		if self.client.data.config.log_file:
 			file = f"logs/{name}/{str(self.client.user.name)}.txt"
 			file_handler = logging.handlers.WatchedFileHandler(file, encoding = "utf-8", mode = "a+")
-			file_handler.setFormatter(CustomFormatter(is_file = True))
+			file_handler.setFormatter(CustomFormatter())
 			logger.addHandler(file_handler)
 			logger.info(f"Created {file}")
 		return logger
